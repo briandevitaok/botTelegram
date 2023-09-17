d4493c236f150e8cdc7f30951bdf27ec4830ccd7
unknown <16tupac16@gmail.com>
2023-09-17 20:43:45 -0300

    img y botones

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import telebot
+from telebot import types
 
 #Conexion con nuestro BOT
-TOKEN = 'TUTOKEN'
+TOKEN = 'TU_TOKEN'
 bot = telebot.TeleBot(TOKEN)
 
 
 
 #Creacion de comandos simples como `/start` y `/help`
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'Hola! Soy tu primer bot creado con Telebot')
@@ -19,9 +19,44 @@ def send_help(message):
     bot.reply_to(message, 'Puedes interactuar conmigo usando comandos. Por ahora, solo respondo a /start y /help')
 
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+
+
+
+@bot.message_handler(commands=['pizza'])
+def send_options(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+
+    #Crando botones
+    btn_si = types.InlineKeyboardButton('Si', callback_data='pizza_si')
+    btn_no = types.InlineKeyboardButton('No', callback_data='pizza_no') 
+
+
+    #Agrega botones al markup
+    markup.add(btn_si, btn_no)
+
+
+    #Enviar mensaje con los botones
+    bot.send_message(message.chat.id, "¿Te gusta la pizza?", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_query(call):
+    if call.data == 'pizza_si':
+        bot.answer_callback_query(call.id, '¡A mi tambien!')
+    elif call.data == 'pizza_no':
+        bot.answer_callback_query(call.id, '¡Bueno cada uno tienes sus gustos!')
+
+@bot.message_handler(commands=['foto'])
+def send_image(message):
+    img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png'
+    bot.send_photo(chat_id=message.chat.id, photo=img_url, caption='Aqui tienes tu imagen')
+
+
+
+
+
 
 
 if __name__ == "__main__":
